32de7206340ce214f82215c181296aeae8de3abf
rafaelsouzars <rafael21.santos@hotmailcom>
2026-02-10 22:14:31 -0300

    :sparkles: feat: Create Sections Content Description: Not create content sections but create scroll effetcs sections and insert a background image. Author: @rafaelsouzars

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,18 +7,18 @@ const NavBar = () => {
   return (
     <nav className='navbar'>
       <div>
-        <Link to='home' smooth={true} offset={-50} duration={500}>
+        <Link containerId='container' to='home' smooth={true} offset={-50} duration={500}>
           <img className='navbar-logo-image' src={logoCuraWhite} alt='Home'/>
-        </Link>        
+        </Link>               
       </div>
       <div>
-        <Link to='home' smooth={true} offset={-50} duration={500}>Home</Link>
-        <Link to='sobre' smooth={true} offset={-50} duration={500}>Sobre</Link>
-        <Link to='modalidades' smooth={true} offset={-50} duration={500}>Modalidades</Link>
-        <Link to='planos' smooth={true} offset={-50} duration={500}>Planos</Link>
-        <Link to='equipe' smooth={true} offset={-50} duration={500}>Equipe</Link>
-        <Link to='galeria' smooth={true} offset={-50} duration={500}>Galeria</Link>
-        <Link to='contato' smooth={true} offset={-50} duration={500}>Contato</Link>
+        <Link containerId='container' to='home' smooth={true} offset={-50} duration={500}>Home</Link>
+        <Link containerId='container' to='sobre' smooth={true} offset={-50} duration={500}>Sobre</Link>
+        <Link containerId='container' to='modalidades' smooth={true} offset={-50} duration={500}>Modalidades</Link>
+        <Link containerId='container' to='planos' smooth={true} offset={-50} duration={500}>Planos</Link>
+        <Link containerId='container' to='equipe' smooth={true} offset={-50} duration={500}>Equipe</Link>
+        <Link containerId='container' to='galeria' smooth={true} offset={-50} duration={500}>Galeria</Link>
+        <Link containerId='container' to='contato' smooth={true} offset={-50} duration={500}>Contato</Link>
       </div>        
     </nav>
   )
@@ -27,13 +27,23 @@ const NavBar = () => {
 export default NavBar
 
 /**
- * <a href='#'>Home</a>
-        <a href='#sobre'>Sobre</a>
-        <a href='#modalidades'>Modalidades</a>
-        <a href='#planos'>Planos</a>
-        <a href='#equipe'>Equipe</a>
-        <a href='#galeria'>Galeria</a>
-        <a href='#contato'>Contato</a>
+  <Link containerId='container' to='home' smooth={true} offset={-50} duration={500}>Home</Link>
+  <Link containerId='container' to='sobre' smooth={true} offset={-50} duration={500}>Sobre</Link>
+  <Link containerId='container' to='modalidades' smooth={true} offset={-50} duration={500}>Modalidades</Link>
+  <Link containerId='container' to='planos' smooth={true} offset={-50} duration={500}>Planos</Link>
+  <Link containerId='container' to='equipe' smooth={true} offset={-50} duration={500}>Equipe</Link>
+  <Link containerId='container' to='galeria' smooth={true} offset={-50} duration={500}>Galeria</Link>
+  <Link containerId='container' to='contato' smooth={true} offset={-50} duration={500}>Contato</Link>
+ */
+
+/**
+  <a className='navbar-link' href='#home'>Home</a>
+  <a className='navbar-link' href='#sobre'>Sobre</a>
+  <a className='navbar-link' href='#modalidades'>Modalidades</a>
+  <a className='navbar-link' href='#planos'>Planos</a>
+  <a className='navbar-link' href='#equipe'>Equipe</a>
+  <a className='navbar-link' href='#galeria'>Galeria</a>
+  <a className='navbar-link' href='#contato'>Contato</a>
  */
 
 /**
